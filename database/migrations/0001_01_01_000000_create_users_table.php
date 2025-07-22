1c2627105e063a187ed5f39d87f82febbf7c03e6
T-Huwa <thomhuwa066@gmail.com>
2025-07-22 15:28:12 +0200

    cerated seed accountant user and role, but not a single route for this mf

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
-            $table->enum('role', ['admin', 'donor', 'sponsor', 'inventory_manager', 'secretary']);
+            $table->enum('role', ['admin', 'donor', 'sponsor', 'inventory_manager', 'secretary', 'accountant']);
             $table->string('profile_photo')->nullable();
             $table->rememberToken();
 
--- database/seeders/SecretaryUserSeeder.php
+++ database/seeders/SecretaryUserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 class SecretaryUserSeeder extends Seeder
 {
@@ -25,5 +26,55 @@ class SecretaryUserSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+
+        User::create([
+            'name' => 'Accountant User',
+            'email' => 'accountant@example.com',
+            'phone' => '555-987-6543',
+            'role' => 'accountant',
+            'sponsor_type' => null,
+            'organization_name' => null,
+            'password' => Hash::make('test1234'),
+        ]);
+
+        User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@example.com',
+            'phone' => '555-987-6543',
+            'role' => 'inventory_manager',
+            'sponsor_type' => null,
+            'organization_name' => null,
+            'password' => Hash::make('test1234'),
+        ]);
+
+        // DB::table('users')->insert([
+        //     'name' => 'Accountant User',
+        //     'email' => 'accountant@example.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('test1234'),
+        //     'phone' => '555-987-6543',
+        //     'role' => 'accountant',
+        //     'profile_photo' => null,
+        //     'sponsor_type' => null,
+        //     'organization_name' => null,
+        //     'remember_token' => Str::random(10),
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+
+        // DB::table('users')->insert([
+        //     'name' => 'Manager User',
+        //     'email' => 'manager@example.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('test1234'),
+        //     'phone' => '555-987-6543',
+        //     'role' => 'manager',
+        //     'profile_photo' => null,
+        //     'sponsor_type' => null,
+        //     'organization_name' => null,
+        //     'remember_token' => Str::random(10),
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
     }
 }
